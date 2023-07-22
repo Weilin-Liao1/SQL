2ed8b35b03ca44b5c603f4f398671e38b029ac80
Weilin-Liao1 <82377749+Weilin-Liao1@users.noreply.github.com>
2023-07-22 22:06:26 +0200

    Delete Sales Performance.sql

--- Sales Performance.sql
+++
@@ -1,8 +0,0 @@
-#Sales Performance
-SELECT 
-salesperson, SUM(sl.Sales)AS sum_sales
-FROM salesperson sp
-JOIN sales sl
-ON sp.EmployeeKey = sl.EmployeeKey
-GROUP BY sp.EmployeeKey
-ORDER BY SUM(sl.Sales)DESC;
